257d4d0cb882fa6e99f103d421ae711ba8ededeb
Jason Gonzalez <151604713+ELCSM-BROZE@users.noreply.github.com>
2026-09-21 13:29:25 -0300

    versel
    host

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-*3ro#sp5^id1c4p)gn+39d&eqqfhc1t&izwafckw621el4&#we
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evaluacion-1-back-end-2026.vercel.app']
 
 
 # Application definition
